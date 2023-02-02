a9dc48b7688ed7d6aa1319d0b7db53b6c32b41e0
simo0loufi <98494370+simo0loufi@users.noreply.github.com>
2023-02-02 22:39:28 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'project';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-	
-?>
